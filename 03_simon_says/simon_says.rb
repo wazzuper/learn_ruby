e87399f5bc5812a3c3dba46e1ab68bbae4fd0016
artem <clifredd@gmail.com>
2017-11-23 17:21:15 +0500

    03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,54 @@
 #write your code here
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, times_of_repeat = 2)
+  result = ''
+
+  while times_of_repeat > 0
+    word += ' ' if word[-1] != ' '
+    word.strip! if times_of_repeat == 1
+    result << word
+    times_of_repeat -= 1
+  end
+
+  result
+end
+
+def start_of_word(word, num)
+  word[0..num-1]
+end
+
+def first_word(words)
+  words.split[0]
+end
+
+def titleize(words)
+  array_of_words = words.split
+  stop_words = %w{ david copperfield war peace bridge river kwai }
+  result = ''
+
+  array_of_words.each do |word|
+    current_index = array_of_words.index(word)
+
+    word.capitalize! if current_index == 0
+    word.capitalize! if stop_words.include?(word)
+
+    if current_index == array_of_words.index(array_of_words.last)
+      word.strip!
+    else
+      word += ' '
+    end
+
+    result << word
+  end
+
+  result
+end
+
+puts titleize('the bridge over the river kwai')
