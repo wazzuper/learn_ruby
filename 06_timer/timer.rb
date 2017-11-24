aae67121700f5644061d152b84af8c070962de13
artem <clifredd@gmail.com>
2017-11-24 18:24:22 +0500

    06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,42 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def time_string
+    before_60_sec = '00:00:'
+    before_3600_sec = '00:'
+    more_than_3600_sec = ''
+    
+    if seconds < 60
+      before_60_sec << '0' if seconds < 10
+      before_60_sec << seconds.to_s
+    elsif seconds >= 60 && seconds < 3600
+      remainder_of_seconds = seconds % 60
+      minutes = (seconds - remainder_of_seconds) / 60
+
+      before_3600_sec << '0' if minutes < 10
+      before_3600_sec << minutes.to_s
+      before_3600_sec << ':'
+      before_3600_sec << '0' if remainder_of_seconds < 10
+      before_3600_sec << remainder_of_seconds.to_s
+    else
+      remainder_of_seconds = seconds % 60
+      minutes = (seconds - remainder_of_seconds) / 60
+      remainder_of_minutes = minutes % 60
+      hours = (minutes - remainder_of_minutes) / 60
+
+      more_than_3600_sec << '0' if hours < 10
+      more_than_3600_sec << hours.to_s
+      more_than_3600_sec << ':'
+      more_than_3600_sec << '0' if remainder_of_minutes < 10
+      more_than_3600_sec << remainder_of_minutes.to_s
+      more_than_3600_sec << ':'
+      more_than_3600_sec << '0' if remainder_of_seconds < 10
+      more_than_3600_sec << remainder_of_seconds.to_s
+    end
+  end
 end
